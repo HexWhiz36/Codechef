95d641bd486bdcbfe00917a36702063e1a98ec36
Shaon shah <127724216+shaon36@users.noreply.github.com>
2023-09-17 02:10:39 +0600

    Add files via upload

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y,n,i;
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        cin>>x>>y;
+        cout<<x+y<<endl;
+    }
+    return 0;
+}
