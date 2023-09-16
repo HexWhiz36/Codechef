5decdfdd4efd3a40c0eca5afb723204bcd5fa067
Shaon shah <127724216+shaon36@users.noreply.github.com>
2023-09-17 02:11:06 +0600

    Add files via upload

---
+++ Ageing.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,x;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>x;
+        cout<<x-10<<endl;
+    }
+    return 0;
+}
