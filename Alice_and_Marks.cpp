a86398acdba206b7dc35970042d7da3bad7e0c84
Shaon shah <127724216+shaon36@users.noreply.github.com>
2023-09-17 02:11:49 +0600

    Add files via upload

---
+++ Alice_and_Marks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+        cin>>x>>y;
+        if(x>=(y*2)) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+
+    return 0;
+}
