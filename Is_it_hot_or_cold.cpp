b9bdb4be795b397c123dafd349b74a5b15d3a3a3
5H40N <shaonshah36@gmail.com>
2023-09-23 09:26:55 +0600

    Add files via upload

---
+++ Is_it_hot_or_cold.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,x;
+    cin>>t;
+    while (t--)
+    {
+        cin>>x;
+        if(x>20) cout<<"HOT"<<endl;
+        else cout<<"COLD"<<endl;
+    }
+    
+    return 0;
+}
