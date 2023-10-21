60ad5eb2ed0dd877cc70336f8088c2a5fca4eac1
5H40N <shaonshah36@gmail.com>
2023-10-21 23:32:46 +0600

    Add files via upload

---
+++ Ezio_and_Guards.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,x,y;
+    cin>>t;
+    while (t--)
+    {
+        cin>>x>>y;
+        if(y<=x) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
