87aeecc86f3397365e226128fd05706fa5103884
5H40N <shaonshah36@gmail.com>
2023-10-29 02:20:13 +0600

    Add files via upload

---
+++ Maximum_Submissions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,x;
+    cin>>t;
+    while (t--)
+    {
+        cin>>x;
+        cout<<x*2<<endl;
+    }
+    
+    return 0;
+}
