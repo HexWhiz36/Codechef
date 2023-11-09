b334e4888cdcd169754bdc7806a0c4a025b5a186
5H40N <shaonshah36@gmail.com>
2023-11-10 05:01:28 +0600

    Add files via upload

---
+++ October_Marathon.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    if(x<3) cout<<"GOLD"<<endl;
+    else if(x>=3 && x<6) cout<<"SILVER"<<endl;
+    else cout<<"BRONZE"<<endl;
+    return 0;
+}
---
+++ Sports_Section.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    if(x<8) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+    return 0;
+}
